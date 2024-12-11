a9f10cb121bd2c27752b1fe99ccd9fd159c522b0
MamaMia07 <kokoszkam@gazeta.pl>
2024-12-11 16:07:00 +0100

    ueue begin

---
+++ message_passing/shoelace.py
@@ -0,0 +1,37 @@
+import re
+import time
+
+# polygon coordinates format: (45,110), (44, 23), (36, 20)
+
+# to extract the points from input
+PTS_REGEX = "\((\d*),(\d*)\)" # inner brackets separate groups of digits, to extract the X and Y in separate groups
+TOTAL_PROCESSES = 8 # number of all processes, eual to nmb. of cores in computer
+
+
+'''area function will accept a list of points from the queue, 
+the cube between the master and the worker threads'''
+def find_area(points_queue):
+    points = []
+    area = 0.0
+    for xy in re.finditer(PTS_REGEX, points_str):
+        points.append((int(xy.group(1)), int(xy.group(2))))  # xy.group(1) and (2) we extract the first and the second group.
+
+    #after the loop we have list of points
+    for i in range(len(points)):
+        a, b = points[i], points[(i+1) % len(points)]
+        '''% len(points)   -   when i+1 goes past the last index of our points list, we wrap around 
+        and go back to the first one and now we can use points A and B to do our calculation.'''
+        area += a[0]*b[1] - a[1]*b[0]
+
+    area = abs(area)/2
+    #print(area)
+
+
+f = open("polygons.txt", "r")
+lines = f.read().splitlines()
+
+start = time.time()
+for line in lines:
+    find_area(line)
+end = time.time()
+print("Time taken: ", end-start)
