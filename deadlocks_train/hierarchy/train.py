193e9046b23ad8556256e7d31efd742412260653
MamaMia07 <kokoszkam@gazeta.pl>
2024-12-13 15:26:55 +0100

    hierarchy

--- deadlocks_train/hierarchy/train.py
+++ deadlocks_train/hierarchy/train.py
@@ -1,19 +1,28 @@
 import time
+'''Use orderings on locks and resources to avoid deadlocks.
+   In Hierarchy, you need to know in advance which locks you are going to use.'''
 
-'''use ordering on how we can look the different intersections in our launcher.'''
 
-#look the different intersections in advance according to train length.
-
+'''-use ordering on how we can lock the different intersections in our launcher
+   -lock the different intersections in advance according to train length.
+'''
 def lock_intersections_in_distance(id, reserve_start, reserve_end, crossings):
-    intersections_to_lock = []
+    # accepted parameters: id of train locking intersections,
+        #reserve_starts the position along the tracks where we want to start reserving all the crossings
+        # and the reserve_end - the position on the tracks where we want to stop reserving these intersections.
+
+    intersections_to_lock = [] #list with all the intersections that are in that reserve_start and reserve_end.
     for crossing in crossings:
         if reserve_end >= crossing.position >= reserve_start and crossing.intersection.locked_by != id:
             intersections_to_lock.append(crossing.intersection)
+    #after we exit this loop, we have a list of intersections that we need to acquire.
 
     # sort these intersections
     #this will cause the intersections to lock list to be sorted by the intersection id.
     intersections_to_lock = sorted(intersections_to_lock, key=lambda it: it.uid)
 
+
+    # in implementation without ordering these intersections, the deadlock will still occur.
     for intersection in intersections_to_lock:
         intersection.mutex.acquire()
         intersection.locked_by = id
